b41c18b0c9119ced50e9416a6903f068c21729c2
Yewon <askges20@kyonggi.ac.kr>
2021-10-14 23:15:21 +0900

    Using Owlbot Dictionary API

--- project02/app.py
+++ project02/app.py
@@ -12,16 +12,19 @@ def main():
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
-    r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-    response = r.json()  # 받아온 정보를 json 형식으로
-    rows = response['RealtimeCityAir']['row']
+    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 658679be993d1b24d8a6919d69f04f488a6a5b7c"})
+    result = r.json()
+    print(result)
     word_receive = request.args.get("word_give")
+    print(word_receive)
+    return render_template("detail.html", word=keyword)
 
-    # return render_template("detail.html", rows=rows, word=word_receive)
-    # url에 ?word_receive=단어
+    # 미세먼지
+    # r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+    # response = r.json()  # 받아온 정보를 json 형식으로
+    # rows = response['RealtimeCityAir']['row']
 
-    return render_template("detail.html", rows=rows, word=keyword)
-    # url에 /키워드
+    # return render_template("detail.html", rows=rows, word=keyword)
 
 
 if __name__ == '__main__':
