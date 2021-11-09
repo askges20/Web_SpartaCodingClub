cbaaf87fea7add75a9098a84b99ee54e70e9ec73
Yewon <askges20@kyonggi.ac.kr>
2021-11-09 22:37:55 +0900

    week3 get restaurant from db / add css, marker info, og tag

--- project03/app.py
+++ project03/app.py
@@ -12,14 +12,21 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
-@app.route('/map')
-def test_map():
-    return render_template("prac_map.html")
-
-@app.route('/matjip', methods=["GET"])
+@app.route('/matjip', methods=['GET'])
 def get_matjip():
-    # 맛집 목록을 반환하는 API
-    return jsonify({'result': 'success', 'matjip_list': []})
+    # 1. 데이터베이스에서 맛집 목록 꺼내오기
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # 2. 클라이언트에 돌려주기
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
+# @app.route('/map')
+# def test_map():
+#     return render_template("prac_map.html")
+#
+# @app.route('/matjip', methods=["GET"])
+# def get_matjip():
+#     # 맛집 목록을 반환하는 API
+#     return jsonify({'result': 'success', 'matjip_list': []})
 
 
 if __name__ == '__main__':
